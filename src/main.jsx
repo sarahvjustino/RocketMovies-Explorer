7ecea90ed6aa09bbcdb0b5b0f226c27bf982403b
Sarah <sarahvictoria230@gmail.com>
2023-09-04 22:08:48 -0300

    feat: created and styled SignIn page

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import GlobalStyle from "./styles/global";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
-import { Profile } from "./pages/Profile";
+import { SignIn } from "./pages/SignIn";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Profile />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>
 );
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,5 +1,25 @@
-import { Container } from "./styles";
+import { FiMail, FiLock } from "react-icons/fi";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { Container, Form, Background } from "./styles";
 
 export function SignIn() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Form>
+        <h1>RocketMovies</h1>
+        <p>Aplicação para acompanhar tudo que assistir.</p>
+
+        <h2>Faça seu login</h2>
+        <Input placeholder="E-mail" type="password" icon={FiMail} />
+        <Input placeholder="Senha" type="password" icon={FiLock} />
+
+        <Button title="Entrar" />
+        <a href="/">Criar conta</a>
+      </Form>
+
+      <Background />
+    </Container>
+  );
 }
