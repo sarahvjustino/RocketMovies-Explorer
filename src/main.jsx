74e70f8955899d23d40139c9268bfe4b14b558e3
Sarah <sarahvictoria230@gmail.com>
2023-12-25 15:34:40 -0300

    feat: created auth hook and SignIn context

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Routes } from "./routes";
 import GlobalStyle from "./styles/global";
 import { ThemeProvider } from "styled-components";
+
+import { AuthProvider } from "./hooks/auth";
+
 import theme from "./styles/theme";
-import { Routes } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Container, Form, Background } from "./styles";
 import { FiMail, FiLock } from "react-icons/fi";
 import { Link } from "react-router-dom";
@@ -5,7 +6,18 @@ import { Link } from "react-router-dom";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
+import { useAuth } from "../../hooks/auth";
+
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { SignIn } = useAuth();
+
+  function handleSignIn() {
+    SignIn({ email, password });
+  }
+
   return (
     <Container>
       <Form>
@@ -13,10 +25,20 @@ export function SignIn() {
         <p>Aplicação para acompanhar tudo que assistir.</p>
 
         <h2>Faça seu login</h2>
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={(event) => setPassword(event.target.value)}
+        />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn} />
         <Link to="/register">Criar conta</Link>
       </Form>
 
